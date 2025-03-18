4599251ba48640dc243f20a61b4710cf7fe0ca8e
hasanozbakir <hasan.h.ozbakir@gmail.com>
2025-03-18 12:05:37 +0300

    feat: create a new branch and make the project setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import { Provider } from 'react-redux';
-import { store } from './redux-store/store.ts';
 
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('Root element not found');
-}
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+    <App />
   </StrictMode>
 );
